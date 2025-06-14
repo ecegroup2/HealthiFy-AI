e381e1266f419164dacfcdae70f1de1ae2a2524d
HealthiFy <projectimposs@gmail.com>
2025-06-14 21:57:59 +0530

    ImageUpload.tsx

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -81,7 +81,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ onImageSelected, isAnalyzing 
   };
 
   return (
-    <Card className="w-full">
+    <Card className="w-full pt-4">
       <CardContent className="p-4">
         <input
           type="file"
